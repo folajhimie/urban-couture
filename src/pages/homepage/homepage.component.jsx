1082bcd1d893d9f5e06717a8e2cf6bbd5981ec38
Folajhimie <blackhemisphere12@gmail.com>
2021-06-09 02:54:44 +0100

    broken our hompage into different components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Directory from '../../component/directory/directory.component';
+import './homepage.styles.scss';
+
+export const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+// export default HomePage;
