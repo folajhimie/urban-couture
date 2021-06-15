e16ce889e4e23d638a130378f497392ce487651e
Folajhimie <blackhemisphere12@gmail.com>
2021-06-15 13:05:24 +0100

    Finished with the SignUp Buttons both the Form Input and the Custom Button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx'
-import Header from './component/header/header.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './component/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route  path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
