f0659029335c98a1519fe523db89bb697ecd19a5
Folajhimie <blackhemisphere12@gmail.com>
2021-06-17 02:58:37 +0100

    storing User Data in our App

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,6 +6,7 @@ import 'firebase/auth'
 const config = {
     apiKey: "AIzaSyBzimM23u9UD5wla0k4KaVyzgSd0jLEG8s",
     authDomain: "urban-couture-34071.firebaseapp.com",
+    databaseURL: "https://urban-couture-34071-default-rtdb.firebaseio.com",
     projectId: "urban-couture-34071",
     storageBucket: "urban-couture-34071.appspot.com",
     messagingSenderId: "939417831835",
@@ -14,6 +15,29 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error){
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
